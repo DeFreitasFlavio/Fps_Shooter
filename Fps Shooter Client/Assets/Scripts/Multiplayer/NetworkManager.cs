6a18fd655056c087216dd3087a2d4698bcf846e6
Farixo <flavio.defreitas@ynov.com>
2022-05-19 10:14:59 +0200

    All

--- Fps Shooter Client/Assets/Scripts/Multiplayer/NetworkManager.cs
+++ Fps Shooter Client/Assets/Scripts/Multiplayer/NetworkManager.cs
@@ -1,23 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using RiptideNetworking;
 using RiptideNetworking.Utils;
-using UnityEngine;
 using System;
+using UnityEngine;
 
 public enum ServerToClientId : ushort
 {
-    playerSpawned = 1,
+    sync = 1,
+    activeScene,
+    playerSpawned,
+    playerMovement,
+    playerHealthChanged,
+    playerActiveWeaponUpdated,
+    playerAmmoChanged,
+    playerDied,
+    playerRespawned,
+    projectileSpawned,
+    projectileMovement,
+    projectileCollided,
+    projectileHitmarker,
 }
-public enum ClientToServerId :ushort
+
+public enum ClientToServerId : ushort
 {
     name = 1,
+    input,
+    switchActiveWeapon,
+    primaryUse,
+    reload,
 }
 
 public class NetworkManager : MonoBehaviour
 {
     private static NetworkManager _singleton;
-
     public static NetworkManager Singleton
     {
         get => _singleton;
@@ -27,18 +41,40 @@ public class NetworkManager : MonoBehaviour
                 _singleton = value;
             else if (_singleton != value)
             {
-                Debug.Log($"{nameof(NetworkManager)} instance already exist, destroying duplicate!");
+                Debug.Log($"{nameof(NetworkManager)} instance already exists, destroying duplicate!");
                 Destroy(value);
             }
         }
-
     }
 
     public Client Client { get; private set; }
 
+    private ushort _serverTick;
+    public ushort ServerTick
+    {
+        get => _serverTick;
+        private set
+        {
+            _serverTick = value;
+            InterpolationTick = (ushort)(value - TicksBetweenPositionUpdates);
+        }
+    }
+    public ushort InterpolationTick { get; private set; }
+    private ushort _ticksBetweenPositionUpdates = 2;
+    public ushort TicksBetweenPositionUpdates
+    {
+        get => _ticksBetweenPositionUpdates;
+        private set
+        {
+            _ticksBetweenPositionUpdates = value;
+            InterpolationTick = (ushort)(ServerTick - value);
+        }
+    }
+
     [SerializeField] private string ip;
     [SerializeField] private ushort port;
-
+    [Space(10)]
+    [SerializeField] private ushort tickDivergenceTolerance = 1;
 
     private void Awake()
     {
@@ -52,12 +88,16 @@ public class NetworkManager : MonoBehaviour
         Client = new Client();
         Client.Connected += DidConnect;
         Client.ConnectionFailed += FailedToConnect;
+        Client.ClientDisconnected += PlayerLeft;
         Client.Disconnected += DidDisconnect;
+
+        ServerTick = TicksBetweenPositionUpdates;
     }
-    
-    private void  FixedUpdate()
+
+    private void FixedUpdate()
     {
         Client.Tick();
+        ServerTick++;
     }
 
     private void OnApplicationQuit()
@@ -80,8 +120,33 @@ public class NetworkManager : MonoBehaviour
         UIManager.Singleton.BackToMain();
     }
 
+    private void PlayerLeft(object sender, ClientDisconnectedEventArgs e)
+    {
+        if (Player.list.TryGetValue(e.Id, out Player player))
+            Destroy(player.gameObject);
+    }
+
     private void DidDisconnect(object sender, EventArgs e)
     {
         UIManager.Singleton.BackToMain();
+        foreach (Player player in Player.list.Values)
+            Destroy(player.gameObject);
+
+        GameLogic.Singleton.UnloadActiveScene();
+    }
+
+    private void SetTick(ushort serverTick)
+    {
+        if (Mathf.Abs(ServerTick - serverTick) > tickDivergenceTolerance)
+        {
+            Debug.Log($"Client tick: {ServerTick} -> {serverTick}");
+            ServerTick = serverTick;
+        }
+    }
+
+    [MessageHandler((ushort)ServerToClientId.sync)]
+    public static void Sync(Message message)
+    {
+        Singleton.SetTick(message.GetUShort());
     }
 }
